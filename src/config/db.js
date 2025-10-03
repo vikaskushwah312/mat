b900cb4c5be988ade6a38683fbc4ce9af061b1b3
vikas <vikas@itsabacus.com>
2025-10-03 12:51:41 +0530

    configration done

--- src/config/db.js
+++ src/config/db.js
@@ -2,12 +2,19 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  // process.env.DB_NAME,
+  // process.env.DB_USER,
+  // process.env.DB_PASSWORD,
+  "matapi",
+  "admin",
+  "Mat&&123456"
+
   {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    // host: process.env.DB_HOST,
+    // port: process.env.DB_PORT,
+    host: "matapi.cjok0ks8mmub.ap-south-1.rds.amazonaws.com"
+    port: 3306,
+
     dialect: 'mysql',
     logging: false, // Set to console.log to see SQL queries
     pool: {
